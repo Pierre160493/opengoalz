617cec80210a5651ce4f3a9b55922775047bef77
Pierre160493 <pierre160493@gmail.com>
2025-04-23 16:21:07 +0200

    Fix date_end calculation in simulate_game_main function to use date_start

--- sql/functions/simulate_game/simulate_game_main.sql
+++ sql/functions/simulate_game/simulate_game_main.sql
@@ -450,7 +450,7 @@ BEGIN
     ------ Store the score
     UPDATE games SET
         -- date_end = date_start + (loc_minute_period_end * INTERVAL '1 minute'),
-        date_end = NOW() + INTERVAL '5 minutes',
+        date_end = date_start + INTERVAL '5 minutes',
         -- date_end = NOW(),
         ---- Score of the game
         score_left = CASE WHEN loc_score_left = -1 THEN 0 ELSE loc_score_left END,
