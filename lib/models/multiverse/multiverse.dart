80161abe383c4c709e0d625c0868091b8f85b9ab
Pierre160493 <pierre160493@gmail.com>
2025-04-27 07:46:57 +0200

    Add isActive property to Multiverse model and update fromMap factory method

--- lib/models/multiverse/multiverse.dart
+++ lib/models/multiverse/multiverse.dart
@@ -13,6 +13,7 @@ class Multiverse {
   final DateTime lastRun;
   final String? error;
   final DateTime dateNextHandling; // New property
+  final bool isActive; // New property
 
   Multiverse({
     required this.id,
@@ -27,6 +28,7 @@ class Multiverse {
     required this.lastRun,
     required this.error,
     required this.dateNextHandling, // New property
+    required this.isActive, // New property
   });
 
   factory Multiverse.fromMap(Map<String, dynamic> map) {
@@ -43,6 +45,7 @@ class Multiverse {
       lastRun: DateTime.parse(map['last_run']).toLocal(),
       error: map['error'],
       dateNextHandling: DateTime.parse(map['date_next_handling']).toLocal(),
+      isActive: map['is_active'], // New property
     );
   }
 
