2ccaa999db4789932c7a021e43f238112927a24d
Pierre160493 <pierre160493@gmail.com>
2025-05-06 11:49:17 +0200

    Update multiverse insertion dates: adjust start dates for 'The Original' and 'The Second' entries to one week earlier

--- sql/queries/RESET_PROJECT.sql
+++ sql/queries/RESET_PROJECT.sql
@@ -35,8 +35,8 @@ BEGIN
     --UPDATE profiles SET id_default_club = NULL;
 
     INSERT INTO public.multiverses (id, date_season_start, speed, name) VALUES 
-    (1, date_trunc('week', NOW()) + INTERVAL '5 days 20 hours', 1, 'The Original'), -- 1 game per week
-    (2, date_trunc('week', NOW()) + INTERVAL '5 days 20 hours', 2, 'The Second'), -- 2 games per week
+    (1, date_trunc('week', NOW()) - INTERVAL '1 week' + INTERVAL '5 days 20 hours', 1, 'The Original'), -- 1 game per week
+    (2, date_trunc('week', NOW()) - INTERVAL '1 week' + INTERVAL '5 days 20 hours', 2, 'The Second'), -- 2 games per week
     (3, date_trunc('week', NOW()) + INTERVAL '20 hours', 7, 'The Lucky Seven'), -- 1 game per day
     (4, date_trunc('week', NOW()) + INTERVAL '20 hours', 14, 'The Double Seven'), -- 2 games per day
     (5, date_trunc('day', NOW()), 168, 'The Very Fast'), -- Every hour
