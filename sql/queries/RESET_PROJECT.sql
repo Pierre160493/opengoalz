7fcc04c62e3db2040adc62a11165112aa1bb3c6f
Pierre160493 <pierre160493@gmail.com>
2025-05-06 11:46:56 +0200

    Update multiverse insertion timestamps: replace CURRENT_DATE with NOW() for accurate time handling

--- sql/queries/RESET_PROJECT.sql
+++ sql/queries/RESET_PROJECT.sql
@@ -35,12 +35,12 @@ BEGIN
     --UPDATE profiles SET id_default_club = NULL;
 
     INSERT INTO public.multiverses (id, date_season_start, speed, name) VALUES 
-    (1, date_trunc('week', CURRENT_DATE) + INTERVAL '5 days 20 hours', 1, 'The Original'), -- 1 game per week
-    (2, date_trunc('week', CURRENT_DATE) + INTERVAL '5 days 20 hours', 2, 'The Second'), -- 2 games per week
-    (3, date_trunc('week', CURRENT_DATE) + INTERVAL '20 hours', 7, 'The Lucky Seven'), -- 1 game per day
-    (4, date_trunc('week', CURRENT_DATE) + INTERVAL '20 hours', 14, 'The Double Seven'), -- 2 games per day
-    (5, date_trunc('day', CURRENT_DATE), 168, 'The Very Fast'), -- Every hour
-    (6, date_trunc('hour', CURRENT_DATE), 1008, 'The Ultra Fast'); -- Every 10 minutes
+    (1, date_trunc('week', NOW()) + INTERVAL '5 days 20 hours', 1, 'The Original'), -- 1 game per week
+    (2, date_trunc('week', NOW()) + INTERVAL '5 days 20 hours', 2, 'The Second'), -- 2 games per week
+    (3, date_trunc('week', NOW()) + INTERVAL '20 hours', 7, 'The Lucky Seven'), -- 1 game per day
+    (4, date_trunc('week', NOW()) + INTERVAL '20 hours', 14, 'The Double Seven'), -- 2 games per day
+    (5, date_trunc('day', NOW()), 168, 'The Very Fast'), -- Every hour
+    (6, date_trunc('hour', NOW()), 1008, 'The Ultra Fast'); -- Every 10 minutes
 
 --     FOR multiverse IN (
 --         SELECT * FROM multiverses
