e271d526d0579c5cbbf5d6c1e6c0b778151cb052
Pierre160493 <pierre160493@gmail.com>
2025-05-14 17:23:37 +0200

    Enhance player handling function: add coach and scout coefficients, update motivation checks, and streamline player transfer notifications

--- sql/functions/main/main_handle_players.sql
+++ sql/functions/main/main_handle_players.sql
@@ -16,6 +16,7 @@ BEGIN
         keeper, defense, passes, playmaking, winger, scoring, freekick,
         motivation, form, stamina, energy, experience,
         loyalty, leadership, discipline, communication, aggressivity, composure, teamwork,
+        coef_coach, coef_scout,
         training_points_used, user_points_available, user_points_used)
     SELECT
         inp_multiverse.date_handling, id, performance_score_real, performance_score_theoretical,
@@ -23,6 +24,7 @@ BEGIN
         keeper, defense, passes, playmaking, winger, scoring, freekick,
         motivation, form, stamina, energy, experience,
         loyalty, leadership, discipline, communication, aggressivity, composure, teamwork,
+        coef_coach, coef_scout,
         training_points_used, user_points_available, user_points_used
     FROM players
     WHERE id_multiverse = inp_multiverse.id
@@ -102,10 +104,121 @@ BEGIN
         stamina = LEAST(100, GREATEST(0,
             stamina + (random() * 20 - 10) + ((70 - stamina) / 10)
             )), -- Random [-10, +10] AND [+7; -3] based on value AND clamped between 0 and 100
-        experience = experience + 0.05
+        experience = experience + 0.05,
+        loyalty = clamp(loyalty + random() - 0.5, 0, 100),
+        leadership = clamp(leadership + random() - 0.5, 0, 100),
+        discipline = clamp(discipline + random() - 0.5, 0, 100),
+        communication = clamp(communication + random() - 0.5, 0, 100),
+        aggressivity = clamp(aggressivity + random() - 0.5, 0, 100),
+        composure = clamp(composure + random() - 0.5, 0, 100),
+        teamwork = clamp(teamwork + random() - 0.5, 0, 100)
     FROM players1
     WHERE players.id_multiverse = inp_multiverse.id
     AND players.id = players1.id;
+
+    ------ Update players stats based on training points
+    WITH player_data AS (
+        SELECT 
+            players.id, -- Player's id
+            --calculate_age(multiverses.speed, players.date_birth) AS age, -- Player's age
+            (25 - calculate_age(multiverses.speed, players.date_birth, inp_multiverse.date_handling))/10 AS coef_age, -- Player's age
+            training_points_available, -- Initial training points
+            training_coef, -- Array of coef for each stat
+            (COALESCE(clubs.training_weight, 1000) / 5000) ^ 0.3 AS staff_coef, -- Value between 0 and 1 [0 => 0, 5000 => 1]
+            training_coef[1]+training_coef[2]+training_coef[3]+training_coef[4]+training_coef[5]+training_coef[6]+training_coef[7] AS sum_training_coef
+        FROM players
+        LEFT JOIN clubs ON clubs.id = players.id_club
+        LEFT JOIN multiverses ON multiverses.id = players.id_multiverse
+        WHERE players.id_multiverse = inp_multiverse.id
+        AND players.date_death IS NULL
+    ), player_data2 AS (
+        SELECT 
+            player_data.*,
+            training_points_available +
+                3 * (0.25 + 0.75 * staff_coef) + -- The more staff_coeff, the closer to 1
+                5 * coef_age
+            --0
+            AS updated_training_points_available, -- Updated training points based on age and staff weight
+            ARRAY(
+                SELECT (1 - staff_coef) + CASE WHEN sum_training_coef = 0 THEN 1.0 ELSE coef / sum_training_coef::float END
+                FROM UNNEST(training_coef) AS coef
+            ) AS updated_training_coef -- Updated training_coef ARRAY
+        FROM player_data
+    ), player_data3 AS (
+        SELECT 
+            player_data2.*,
+            (SELECT SUM(value) FROM UNNEST(updated_training_coef) AS value) AS total_sum
+        FROM player_data2
+    ), final_data AS (
+        SELECT 
+            player_data3.*,
+            ARRAY(
+                SELECT updated_training_points_available * value / total_sum
+                FROM UNNEST(updated_training_coef) AS value
+            ) AS normalized_training_coef
+        FROM player_data3
+    )
+    UPDATE players SET
+        keeper = GREATEST(0,
+            keeper + 
+            CASE WHEN normalized_training_coef[1] > 0 THEN
+                normalized_training_coef[1] * (1 - (keeper / 100))
+            ELSE normalized_training_coef[1] END),
+        defense = GREATEST(0,
+            defense + 
+            CASE WHEN normalized_training_coef[2] > 0 THEN
+                normalized_training_coef[2] * (1 - (defense / 100))
+            ELSE normalized_training_coef[2] END),
+        passes = GREATEST(0,
+            passes + 
+            CASE WHEN normalized_training_coef[3] > 0 THEN
+                normalized_training_coef[3] * (1 - (passes / 100))
+            ELSE normalized_training_coef[3] END),
+        playmaking = GREATEST(0,
+            playmaking + 
+            CASE WHEN normalized_training_coef[4] > 0 THEN
+                normalized_training_coef[4] * (1 - (playmaking / 100))
+            ELSE normalized_training_coef[4] END),
+        winger = GREATEST(0,
+            winger + 
+            CASE WHEN normalized_training_coef[5] > 0 THEN
+                normalized_training_coef[5] * (1 - (winger / 100))
+            ELSE normalized_training_coef[5] END),
+        scoring = GREATEST(0,
+            scoring + 
+            CASE WHEN normalized_training_coef[6] > 0 THEN
+                normalized_training_coef[6] * (1 - (scoring / 100))
+            ELSE normalized_training_coef[6] END),
+        freekick = GREATEST(0,
+            freekick + 
+            CASE WHEN normalized_training_coef[7] > 0 THEN
+                normalized_training_coef[7] * (1 - (freekick / 100))
+            ELSE normalized_training_coef[7] END),
+        training_points_available = 0,
+        training_points_used = training_points_used + updated_training_points_available
+    FROM final_data
+    WHERE players.id = final_data.id;
+
+    ------ Calculate player performance score
+    UPDATE players SET
+        performance_score_real = players_calculate_player_best_weight(
+            ARRAY[keeper, defense, playmaking, passes, scoring, freekick, winger,
+            -- motivation, form, experience, energy, stamina]),
+            motivation, form, experience, 100, stamina]),
+        performance_score_theoretical = players_calculate_player_best_weight(
+            ARRAY[keeper, defense, playmaking, passes, scoring, freekick, winger,
+            100, 100, experience, 100, 100]),
+        expenses_target = FLOOR(50 +
+            1 * calculate_age(inp_multiverse.speed, date_birth, inp_multiverse.date_handling) +
+            GREATEST(keeper, defense, playmaking, passes, winger, scoring, freekick) / 2 +
+            (keeper + defense + passes + playmaking + winger + scoring + freekick) / 4
+            + (coef_coach + coef_scout) / 2),
+        coef_coach = FLOOR((
+            loyalty + 2 * leadership + 2 * discipline + 2 * communication + 2 * composure + teamwork) / 10),
+        coef_scout = FLOOR((
+            2 * loyalty + leadership + discipline + 3 * communication + 2 * composure + teamwork) / 10)
+    WHERE id_multiverse = inp_multiverse.id
+    AND date_death IS NULL;
 
     ------ If player's motivation is too low, risk of leaving club
     FOR rec_player IN (
@@ -205,118 +318,6 @@ BEGIN
 
         END IF;
     END LOOP;
-
-    ------ Update players stats based on training points
-    WITH player_data AS (
-        SELECT 
-            players.id, -- Player's id
-            --calculate_age(multiverses.speed, players.date_birth) AS age, -- Player's age
-            (25 - calculate_age(multiverses.speed, players.date_birth, inp_multiverse.date_handling))/10 AS coef_age, -- Player's age
-            training_points_available, -- Initial training points
-            training_coef, -- Array of coef for each stat
-            (COALESCE(clubs.training_weight, 1000) / 5000) ^ 0.3 AS staff_coef, -- Value between 0 and 1 [0 => 0, 5000 => 1]
-            training_coef[1]+training_coef[2]+training_coef[3]+training_coef[4]+training_coef[5]+training_coef[6]+training_coef[7] AS sum_training_coef
-        FROM players
-        LEFT JOIN clubs ON clubs.id = players.id_club
-        LEFT JOIN multiverses ON multiverses.id = players.id_multiverse
-        WHERE players.id_multiverse = inp_multiverse.id
-        AND players.date_death IS NULL
-    ), player_data2 AS (
-        SELECT 
-            player_data.*,
-            training_points_available +
-                3 * (0.25 + 0.75 * staff_coef) + -- The more staff_coeff, the closer to 1
-                5 * coef_age
-            --0
-            AS updated_training_points_available, -- Updated training points based on age and staff weight
-            ARRAY(
-                SELECT (1 - staff_coef) + CASE WHEN sum_training_coef = 0 THEN 1.0 ELSE coef / sum_training_coef::float END
-                FROM UNNEST(training_coef) AS coef
-            ) AS updated_training_coef -- Updated training_coef ARRAY
-        FROM player_data
-    ), player_data3 AS (
-        SELECT 
-            player_data2.*,
-            (SELECT SUM(value) FROM UNNEST(updated_training_coef) AS value) AS total_sum
-        FROM player_data2
-    ), final_data AS (
-        SELECT 
-            player_data3.*,
-            ARRAY(
-                SELECT updated_training_points_available * value / total_sum
-                FROM UNNEST(updated_training_coef) AS value
-            ) AS normalized_training_coef
-        FROM player_data3
-    )
-    UPDATE players SET
-        keeper = GREATEST(0,
-            keeper + 
-            CASE WHEN normalized_training_coef[1] > 0 THEN
-                normalized_training_coef[1] * (1 - (keeper / 100))
-            ELSE normalized_training_coef[1] END),
-        defense = GREATEST(0,
-            defense + 
-            CASE WHEN normalized_training_coef[2] > 0 THEN
-                normalized_training_coef[2] * (1 - (defense / 100))
-            ELSE normalized_training_coef[2] END),
-        passes = GREATEST(0,
-            passes + 
-            CASE WHEN normalized_training_coef[3] > 0 THEN
-                normalized_training_coef[3] * (1 - (passes / 100))
-            ELSE normalized_training_coef[3] END),
-        playmaking = GREATEST(0,
-            playmaking + 
-            CASE WHEN normalized_training_coef[4] > 0 THEN
-                normalized_training_coef[4] * (1 - (playmaking / 100))
-            ELSE normalized_training_coef[4] END),
-        winger = GREATEST(0,
-            winger + 
-            CASE WHEN normalized_training_coef[5] > 0 THEN
-                normalized_training_coef[5] * (1 - (winger / 100))
-            ELSE normalized_training_coef[5] END),
-        scoring = GREATEST(0,
-            scoring + 
-            CASE WHEN normalized_training_coef[6] > 0 THEN
-                normalized_training_coef[6] * (1 - (scoring / 100))
-            ELSE normalized_training_coef[6] END),
-        freekick = GREATEST(0,
-            freekick + 
-            CASE WHEN normalized_training_coef[7] > 0 THEN
-                normalized_training_coef[7] * (1 - (freekick / 100))
-            ELSE normalized_training_coef[7] END),
-        training_points_available = 0,
-        training_points_used = training_points_used + updated_training_points_available,
-        -- Randomly update the mental stats
-        loyalty = clamp(loyalty + random() - 0.5, 0, 100),
-        leadership = clamp(leadership + random() - 0.5, 0, 100),
-        discipline = clamp(discipline + random() - 0.5, 0, 100),
-        communication = clamp(communication + random() - 0.5, 0, 100),
-        aggressivity = clamp(aggressivity + random() - 0.5, 0, 100),
-        composure = clamp(composure + random() - 0.5, 0, 100),
-        teamwork = clamp(teamwork + random() - 0.5, 0, 100)
-    FROM final_data
-    WHERE players.id = final_data.id;
-
-    ------ Calculate player performance score
-    UPDATE players SET
-        performance_score_real = players_calculate_player_best_weight(
-            ARRAY[keeper, defense, playmaking, passes, scoring, freekick, winger,
-            -- motivation, form, experience, energy, stamina]),
-            motivation, form, experience, 100, stamina]),
-        performance_score_theoretical = players_calculate_player_best_weight(
-            ARRAY[keeper, defense, playmaking, passes, scoring, freekick, winger,
-            100, 100, experience, 100, 100]),
-        expenses_target = FLOOR(50 +
-            1 * calculate_age(inp_multiverse.speed, date_birth, inp_multiverse.date_handling) +
-            GREATEST(keeper, defense, playmaking, passes, winger, scoring, freekick) / 2 +
-            (keeper + defense + passes + playmaking + winger + scoring + freekick) / 4
-            + (coef_coach + coef_scout) / 2),
-        coef_coach = FLOOR((
-            loyalty + 2 * leadership + 2 * discipline + 2 * communication + 2 * composure + teamwork) / 10),
-        coef_scout = FLOOR((
-            2 * loyalty + leadership + discipline + 3 * communication + 2 * composure + teamwork) / 10)
-    WHERE id_multiverse = inp_multiverse.id
-    AND date_death IS NULL;
 
 END;
 $function$
