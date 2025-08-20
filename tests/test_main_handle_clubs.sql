8516535d53013024e8426193c5bf6cedb2e2e694
Pierre160493 <pierre160493@gmail.com>
2025-08-20 08:49:04 +0200

    Test scenarios for main_handle_clubs

--- tests/test_main_handle_clubs.sql
+++ tests/test_main_handle_clubs.sql
@@ -3,27 +3,20 @@ BEGIN;
 
 DO $$
 DECLARE
-    v_club_id INT; -- Variable for the selected club
+    v_club_id INT := 3577; -- Variable for the selected club
     v_player_id INT; -- Variable for the selected player
 
-    -- Club variables (inputs)
-    v_club_cash INT := 3000;
-    v_club_revenues_sponsors INT := 99;
-    v_club_expenses_training_target INT := 1000;
-    v_club_expenses_scouts_target INT := 2000;
-
-    -- Player variables (inputs)
-    v_expenses_expected INT := 100;
-    v_expenses_missed_to_pay_in_priority INT := 0;
-    v_expenses_missed INT := 0;
-    v_expenses_payed INT;
-
-    -- Club variables (expected results)
+    -- Club variables
+    v_club_cash INT;
     v_club_expenses_players INT;
     v_club_expenses_staff INT;
     v_club_expenses_training_applied INT;
     v_club_expenses_scouts_applied INT;
     v_club_expenses_tax INT;
+
+    -- Player variables
+    v_expenses_payed INT;
+    v_expenses_missed INT;
 
     rec_scenario RECORD; -- Record for the scenarios
     rec_multiverse RECORD; -- Record for the multiverse
@@ -34,12 +27,12 @@ BEGIN
     SELECT * INTO rec_multiverse FROM multiverses WHERE id = 1 AND is_active = TRUE;
 
     -- Get a random club id (id_multiverse = 1)
-    SELECT id INTO v_club_id
-    FROM clubs
-    WHERE id_multiverse = rec_multiverse.id
-    AND number_players > 5
-    ORDER BY random()
-    LIMIT 1;
+    -- SELECT id INTO v_club_id
+    -- FROM clubs
+    -- WHERE id_multiverse = rec_multiverse.id
+    -- AND number_players > 5
+    -- ORDER BY random()
+    -- LIMIT 1;
 
     -- Select a player from the club
     SELECT id INTO v_player_id FROM players WHERE id_club = v_club_id LIMIT 1;
@@ -54,52 +47,50 @@ BEGIN
     FOR rec_scenario IN
         SELECT * FROM (VALUES
             -- Scenario 1: Club has enough cash for all expenses
-            (5000, -- Club's initial cash
+            (1, 5000, -- Club's initial cash
             5000, -- Revenues from sponsors
-            1000, -- Training expenses target
-            2000, -- Scouts expenses target
+            2000, -- Training expenses target
+            1000, -- Scouts expenses target
             100, -- Expected player expenses
             100, -- Missed expenses
             0) -- Missed expenses to pay in priority
-            -- Scenario 2: Club has enough cash for training and scouts, but not for all
-            --(5000, 0, 500, 1000, 200, 10, 5),  -- scenario 2
-            --(100, 0, 0, 0, 10, 0, 0)           -- scenario 3
-        ) AS t(club_cash, revenues_sponsors, expenses_training_target, expenses_scouts_target, expenses_expected, expenses_missed, expenses_missed_to_pay_in_priority)
+            -- Scenario 2: Club cant pay all expenses
+            ,(2, 0, -- Club's initial cash
+            99, -- Revenues from sponsors
+            500, -- Training expenses target
+            1000, -- Scouts expenses target
+            200, -- Expected player expenses
+            10, -- Missed expenses
+            5)  -- Missed expenses to pay in priority
+            -- Scenario 3: Club cant pay all expenses
+            ,(3, 0, -- Club's initial cash
+            150, -- Revenues from sponsors
+            500, -- Training expenses target
+            1000, -- Scouts expenses target
+            100, -- Expected player expenses
+            100, -- Missed expenses
+            5)  -- Missed expenses to pay in priority
+        ) AS t(number, club_cash, revenues_sponsors, expenses_training_target, expenses_scouts_target, expenses_expected, expenses_missed, expenses_missed_to_pay_in_priority)
     LOOP
+    RAISE NOTICE '###### Running scenario: %', rec_scenario.number;
 
-        -- Update the club's fields with the scenario values
-        v_club_cash := rec_scenario.club_cash;
-        v_club_revenues_sponsors := rec_scenario.revenues_sponsors;
-        v_club_expenses_training_target := rec_scenario.expenses_training_target;
-        v_club_expenses_scouts_target := rec_scenario.expenses_scouts_target;
-
-        -- Update the player's fields with the scenario values
-        v_expenses_expected := rec_scenario.expenses_expected;
-        v_expenses_missed := rec_scenario.expenses_missed;
-        v_expenses_missed_to_pay_in_priority := rec_scenario.expenses_missed_to_pay_in_priority;
-
-        -- Print the scenario being tested
-        RAISE NOTICE '###### Running scenario: %', rec_scenario;
-
-        -- Update the club's fields
+        -- Directly update the club's fields with the scenario values
         UPDATE clubs SET
-            cash = v_club_cash,
-            -- Revenues
-            revenues_sponsors = v_club_revenues_sponsors,
+            cash = rec_scenario.club_cash,
+            revenues_sponsors = rec_scenario.revenues_sponsors,
             revenues_transfers_done = 0,
             revenues_transfers_expected = 0,
-            -- Expenses
-            expenses_training_target = v_club_expenses_training_target,
-            expenses_scouts_target = v_club_expenses_scouts_target,
+            expenses_training_target = rec_scenario.expenses_training_target,
+            expenses_scouts_target = rec_scenario.expenses_scouts_target,
             expenses_transfers_done = 0,
             expenses_transfers_expected = 0
         WHERE id = v_club_id;
 
-        -- Update the player's fields
+        -- Directly update the player's fields with the scenario values
         UPDATE players SET
-            expenses_expected = v_expenses_expected,
-            expenses_missed = v_expenses_missed,
-            expenses_missed_to_pay_in_priority = v_expenses_missed_to_pay_in_priority,
+            expenses_expected = rec_scenario.expenses_expected,
+            expenses_missed = rec_scenario.expenses_missed,
+            expenses_missed_to_pay_in_priority = rec_scenario.expenses_missed_to_pay_in_priority,
             expenses_payed = 0
         WHERE id = v_player_id;
 
@@ -111,18 +102,14 @@ BEGIN
         RAISE NOTICE 'Player: %', (SELECT row_to_json(players) FROM players WHERE id = v_player_id);
 
         -- Calculate the expected values after the function call
-        v_club_cash := v_club_cash + v_club_revenues_sponsors;
+        v_club_cash := rec_scenario.club_cash + rec_scenario.revenues_sponsors;
 
         -- Player's expenses payed
         v_expenses_payed := LEAST(
             v_club_cash, 
-            v_expenses_expected + v_expenses_missed);
-        v_expenses_missed := v_expenses_missed + v_expenses_payed - v_expenses_expected;
-
-        -- Player's expenses missed to pay in priority
-        IF v_expenses_missed_to_pay_in_priority > 0 THEN
-            v_expenses_missed := GREATEST(0, v_expenses_missed - v_expenses_missed_to_pay_in_priority);
-        END IF;
+            rec_scenario.expenses_expected + rec_scenario.expenses_missed);
+        -- Player's expenses missed left after weekly payment
+        v_expenses_missed := rec_scenario.expenses_missed + rec_scenario.expenses_expected - v_expenses_payed;
 
         -- Club's cash after paying player expenses
         v_club_cash := v_club_cash - v_expenses_payed;
@@ -130,10 +117,10 @@ BEGIN
         -- Club's other expenses
         v_club_expenses_training_applied := LEAST(
             v_club_cash, 
-            v_club_expenses_training_target);
+            rec_scenario.expenses_training_target);
         v_club_expenses_scouts_applied := LEAST(
             v_club_cash - v_club_expenses_training_applied,
-            v_club_expenses_scouts_target);
+            rec_scenario.expenses_scouts_target);
         v_club_expenses_tax := (v_club_cash - v_club_expenses_training_applied - v_club_expenses_scouts_applied) * 0.05;
 
         -- Club's cash after all expenses
@@ -158,9 +145,6 @@ BEGIN
         IF rec_tmp.cash != v_club_cash THEN
             RAISE EXCEPTION 'Club cash incorrect: expected %, got %', v_club_cash, rec_tmp.cash;
         END IF;
-        IF rec_tmp.revenues_sponsors != v_club_revenues_sponsors THEN
-            RAISE EXCEPTION 'Club revenues_sponsors incorrect: expected %, got %', v_club_revenues_sponsors, rec_tmp.revenues_sponsors;
-        END IF;
         IF rec_tmp.expenses_training_applied != v_club_expenses_training_applied THEN
             RAISE EXCEPTION 'Club expenses_training_applied incorrect: expected %, got %', v_club_expenses_training_applied, rec_tmp.expenses_training_applied;
         END IF;
@@ -170,8 +154,8 @@ BEGIN
         IF rec_tmp.expenses_tax != v_club_expenses_tax THEN
             RAISE EXCEPTION 'Club expenses_tax incorrect: expected %, got %', v_club_expenses_tax, rec_tmp.expenses_tax;
         END IF;
-        IF rec_tmp.expenses_players != v_expenses_expected THEN
-            RAISE EXCEPTION 'Club expenses_players incorrect: expected %, got %', v_expenses_expected, rec_tmp.expenses_players;
+        IF rec_tmp.expenses_players != v_expenses_payed THEN
+            RAISE EXCEPTION 'Club expenses_players incorrect: expected %, got %', v_expenses_payed, rec_tmp.expenses_players;
         END IF;
         IF rec_tmp.expenses_staff != 0 THEN
             RAISE EXCEPTION 'Club expenses_staff incorrect: expected %, got %', 0, rec_tmp.expenses_staff;
